9ac345097e35bffbd110fe3bac65e13de4d51eb3
Luis Revilla <luisrevillameza@gmail.com>
2018-11-20 17:18:50 -0500

    Reversed array of commits. Last goes first

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        {commits.map(com => {
+        {commits.reverse().map(com => {
           return (
             <div className="Commit">
               <h1>Project: {com.project}</h1>
