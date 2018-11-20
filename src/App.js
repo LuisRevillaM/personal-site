ba1c041fae62d73589809e5e32bf90982d683e95
Luis Revilla <luisrevillameza@gmail.com>
2018-11-20 12:47:43 -0500

    Built list of commits on main component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import commits from "./commits.json";
 
 class App extends Component {
   render() {
-    return (<div className="Senior">
-      <div className="App">
-<div className="Top"></div>
-<div className="F2"></div>
-<div className="F3"></div>
-<div className="F4"></div>
-<div className="F5"></div>
-      <div className="Left">
-</div>
-        <div className="Middle">
-        <div className="F1"></div>
-          <div className="triangle-bottomleft"></div>
-          <div className="triangle-bottomright"></div>
-
-
-
-</div>
-
-<div className="Right"> </div>
-
-<div className="MainText"><h1 className="Title">Luis Revilla</h1><div className="SubTitle">Software Engineer based in New York</div><div className="SubTitle"> +3 years of JavaScript experience</div> </div>
-
-
-
-
-      </div></div>
+    return (
+      <div>
+        {commits.map(com => {
+          return (
+            <div className="Commit">
+              <h1>Project: {com.project}</h1>
+              <h2>On branch: {com.branch}</h2>
+              <h2> Commit: {com.commit}</h2>
+            </div>
+          );
+        })}
+      </div>
     );
   }
 }
