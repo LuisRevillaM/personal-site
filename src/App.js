a98a8a3ab77a9f0de94da1a042e430cfb8e74314
Luis Revilla <luisrevillameza@gmail.com>
2018-11-22 08:54:10 -0500

    commit with dates now!

--- src/App.js
+++ src/App.js
@@ -7,15 +7,29 @@ class App extends Component {
     return (
       <div className="app-parent">
 
+        <div className="welcome"><div>Luis Revilla.</div>
+        <div>Fullstack developer based in Manhattan.</div></div>
+
+        <div className="feature-block">
+
+        <div className="feature">3+ years writing Javascript, both for the web and Node.js programs.
+</div><div className="feature">2+ years programming UIs with React. 1+ year building mobile applications with React Native.</div>
+<div className="feature">Strong intuition for design.</div><div className="feature">
+Exceptional ability to learn.</div>
+
+
+        </div>
+
             <div>
+
 
             {commits.reverse().map(com => {
               return (
                 <div className="Commit">
-                  <h1>Project: {com.project}</h1>
-                  <h2>On branch: {com.branch}</h2>
-                  <h2> Commit: {com.commit}</h2>
-                  <h2> Remote: {com.remote} </h2>
+                  <div>Project: {com.project}</div>
+                  <div>On branch: {com.branch}</div>
+                  <div> Commit: {com.commit}</div>
+                  <div> Remote: {com.remote} </div>
                 </div>
               );
             })}
