f6b49fd4edd168177f129195165695d85d33ed52
Luis Revilla <luisrevillameza@gmail.com>
2018-11-21 20:27:52 -0500

    I love you baby

--- src/App.js
+++ src/App.js
@@ -5,18 +5,24 @@ import commits from "./commits.json";
 class App extends Component {
   render() {
     return (
-      <div>
-        {commits.reverse().map(com => {
-          return (
-            <div className="Commit">
-              <h1>Project: {com.project}</h1>
-              <h2>On branch: {com.branch}</h2>
-              <h2> Commit: {com.commit}</h2>
-              <h2> Remote: {com.remote} </h2>
-            </div>
-          );
-        })}
+      <div className="app-parent">
+
+            <div>
+
+            {commits.reverse().map(com => {
+              return (
+                <div className="Commit">
+                  <h1>Project: {com.project}</h1>
+                  <h2>On branch: {com.branch}</h2>
+                  <h2> Commit: {com.commit}</h2>
+                  <h2> Remote: {com.remote} </h2>
+                </div>
+              );
+            })}
+          </div>
+
       </div>
+
     );
   }
 }
