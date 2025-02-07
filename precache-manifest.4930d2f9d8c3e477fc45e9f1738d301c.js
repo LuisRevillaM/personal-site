e227296d2b4c1e9d2fca729f810b63509f2e5a68
luisrevillam <luisrevillameza@gmail.com>
2025-02-07 06:48:07 -0600

    Updates

---
+++ precache-manifest.4930d2f9d8c3e477fc45e9f1738d301c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7f84cc994ef5672f1a9",
+    "url": "/static/css/main.185d03f1.chunk.css"
+  },
+  {
+    "revision": "b7f84cc994ef5672f1a9",
+    "url": "/static/js/main.b7f84cc9.chunk.js"
+  },
+  {
+    "revision": "e614a9c1ee9bb54d1d84",
+    "url": "/static/js/1.e614a9c1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "39a777d871d64bedd77f1234f8b14480",
+    "url": "/index.html"
+  }
+];
